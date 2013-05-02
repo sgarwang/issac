3970b0ff3e8589b698e4369168cee28758dd2a94
sgarwang <sgarwang@gmail.com>
2013-05-02 17:37:52 +0800

    Feature: first rails

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ issac
 
 Web page for GLC Taiwan located in Taipei city, Taiwan.
 
+This site was developed using Ruby on Rails.
+We need volunteers.
+For anyone who are interested in learning, please contact me.
+
+Contributor:
+  * sgarwang@gmail.com
 
 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Issac::Application.config.secret_token = 'cb5a4bcf2449be907057a54cd1c9ce6503864441965b40d973cabac28d382924831aa2ebd7be44b22fb7ccf1cb3339478703b70a511de8d2b39206ad492801c2'
